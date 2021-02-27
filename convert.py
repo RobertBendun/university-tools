e8e3bb66654756500347aef4824d276716273cd6
Robert Bendun <robert@bendun.cc>
2021-02-27 21:56:29 +0100

    Added video optimization script

---
+++ convert.py
@@ -0,0 +1,53 @@
+#!/bin/python3 convert.py
+
+from os import path, listdir, _exit
+from subprocess import run
+from sys import argv, exit
+
+# Extensions that are used to recognize input format. Must not include 
+# output format extension
+Input_Format_Extensions = ['.mp4']
+Output_Format_Extension = '.mkv'
+
+def convert(src, dest):
+	"Convert src file into dest file via ffmpeg"
+	command = f'ffmpeg -i {src} -c:v libx265 -preset slow {dest}'
+	print('$ ' + command)
+	return run(command, shell=True).returncode == 0
+
+def process_file(input):
+	"If input file does not have matching output file, create it via convert function."
+	output = path.splitext(input)[0] + Output_Format_Extension
+	if not convert(input, output):
+		print(f'>>> Failed processing {input}')
+		exit(1)
+
+def process_directory(directory = '.'):
+	"Process each file with input format extension in given directory"
+	for filename in listdir(directory):
+		if any(filename.endswith(ext) for ext in Input_Format_Extensions):
+			process_file(filename)
+
+def main():
+	# Unify arguments behaviour with two potential calling methods:
+	# 	./convert.py via #!
+	# 	python convert.py
+	this_file = path.abspath(__file__)
+	args = [p for p in argv[1:] if path.abspath(p) != this_file]
+
+	if len(args) == 0:
+		process_directory()
+		return
+
+	for entry in args:
+		if path.isdir(entry):
+			process_directory(entry)
+		else:
+			process_file(entry)
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt as ki:
+		try: exit(1)
+		except: _exit(1)
